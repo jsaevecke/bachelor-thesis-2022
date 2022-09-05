dccfe70d521cbf0c8aaedd980c6a964baa21adee
Julien Saevecke <Julien.Saevecke@udo.edu>
2022-09-05 14:45:12 +0200

    add script to run evaluations easily

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+iterations=$1
+# shellcheck disable=SC2034
+for iteration in $(seq "${iterations}"); do
+    microk8s kubectl delete deployment coffee-sul -n saevecke
+    microk8s kubectl delete scaledobject rabbitmq-scaledobject -n saevecke
+    for index in "$@:3"
+    do
+        microk8s kubectl apply -f "$index"
+        sleep 10
+    done
+    java -DSAVE_RESULTS_TO_FILE=true -DFILENAME_PREFIX="${iteration}_${2}" -jar ./learner.jar --spring.rabbitmq.port=30640
+done
