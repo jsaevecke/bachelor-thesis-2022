bf7b6ca5d6a165ad301084135c020a11e84fdea3
Julien Saevecke <Julien.Saevecke@udo.edu>
2022-09-05 15:00:53 +0200

    fix kubectl command for microk8s

--- monitor-replicas.sh
+++ monitor-replicas.sh
@@ -3,6 +3,6 @@ start_time=$SECONDS
 while sleep 1;
 do
   elapsed=$(( SECONDS - start_time ))
-  replica_count=$(kubectl get deployment coffee-sul -n saevecke -o=jsonpath='{.status.replicas}')
+  replica_count=$(microk8s kubectl get deployment coffee-sul -n saevecke -o=jsonpath='{.status.replicas}')
   echo "$elapsed $replica_count" >> "$1"
 done
