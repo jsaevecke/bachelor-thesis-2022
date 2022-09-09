190cf38281dbe55ca90d1e4330aafd50c69e1957
Julien Saevecke <Julien.Saevecke@udo.edu>
2022-09-10 00:32:41 +0200

    add cooldown period and enable pod monitoring for scaling experiments

--- run-all-experiments.sh
+++ run-all-experiments.sh
@@ -9,8 +9,8 @@ source run_experiment.sh 3 "${1}_fixed_12" 25 false ./resources/deployment/graal
 source run_experiment.sh 3 "${1}_fixed_14" 25 false ./resources/deployment/graalvm/setups/fixed/deployment-14.yaml
 source run_experiment.sh 3 "${1}_fixed_16" 30 false ./resources/deployment/graalvm/setups/fixed/deployment-16.yaml
 
-source run_experiment.sh 3 "${1}_ql1_min0" 10 false ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min0.yaml
-source run_experiment.sh 3 "${1}_ql1_min1" 10 false ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min1.yaml
-source run_experiment.sh 3 "${1}_ql1_min2" 15 false ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min2.yaml
-source run_experiment.sh 3 "${1}_ql1_min3" 15 false ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min3.yaml
-source run_experiment.sh 3 "${1}_ql1_min4" 15 false ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min4.yaml
+source run_experiment.sh 3 "${1}_ql1_min0" 10 true ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min0.yaml
+source run_experiment.sh 3 "${1}_ql1_min1" 10 true ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min1.yaml
+source run_experiment.sh 3 "${1}_ql1_min2" 15 true ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min2.yaml
+source run_experiment.sh 3 "${1}_ql1_min3" 15 true ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min3.yaml
+source run_experiment.sh 3 "${1}_ql1_min4" 15 true ./resources/deployment/graalvm/setups/keda/deployment-0.yaml ./resources/deployment/graalvm/setups/keda/queue-length/ql1_min4.yaml
